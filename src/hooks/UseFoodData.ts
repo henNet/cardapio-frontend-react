01b3db7fa6d857ca7cf2e42b12913006de78c759
henNet <dmf.n123@gmail.com>
2024-06-02 02:08:17 -0300

    Modicado URL para API hospedado na vercel

--- src/hooks/UseFoodData.ts
+++ src/hooks/UseFoodData.ts
@@ -2,7 +2,8 @@ import axios, { AxiosPromise } from "axios";
 import FoodData from "../interface/FoodData";
 import { useQuery } from "@tanstack/react-query";
 
-const API_URL = "http://localhost:8080/food";
+// const API_URL = "http://localhost:8080/food";
+const API_URL = "https://cardapio-nodejs-postgres.vercel.app/food";
 
 const fetchData = async (): AxiosPromise<FoodData[]> => {
     const response = axios.get(API_URL);
--- src/hooks/UseFoodMutate.ts
+++ src/hooks/UseFoodMutate.ts
@@ -2,7 +2,8 @@ import axios, { AxiosPromise } from "axios";
 import FoodData from "../interface/FoodData";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 
-const API_URL = "http://localhost:8080/food";
+// const API_URL = "http://localhost:8080/food";
+const API_URL = "https://cardapio-nodejs-postgres.vercel.app/food";
 
 const postData = async (data: FoodData): AxiosPromise<any> => {
     const response = axios.post(API_URL, data);
